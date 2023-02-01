674c26176fee5265f3a04e1ad6b91eb7e690c5d6
Clayton Roberts <clayton.roberts@theverygroup.com>
2023-02-01 10:22:41 +0000

    homescreen partial under construction

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -12,8 +12,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 20.0, top: 200),
+            height: 100,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage("assets/icons/construction.png"))),
+          ),
+          const Text("Home Screen Under Construction")
+        ],
+      ),
     );
   }
 }
